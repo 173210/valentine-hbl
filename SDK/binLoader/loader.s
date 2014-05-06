f386bbc2df0685cd445f349026b1d269abe6bd41
root.3.173210@gmail.com <root.3.173210@gmail.com@ae707b24-f6f8-a420-3b91-9542b9ad9fd8>
2014-05-06 01:45:52 +0000

    Light binary loader
    git-svn-id: https://valentine-hbl.googlecode.com/svn/trunk@199 ae707b24-f6f8-a420-3b91-9542b9ad9fd8

--- SDK/binLoader/loader.s
+++ SDK/binLoader/loader.s
@@ -1,37 +1,22 @@
-/* by Wololo (adapted from Gripshift loader by Matiaz) */
-
+# by 173210 and Wololo (adapted from Gripshift loader by Matiaz)
+ 
 .set noat
 .set noreorder
-
-
-nop
-
-
-addiu $a0, $ra, 0x60	/* filename. If ou change this value, move it in the savegame */
-nop
+ 
+ 
+addiu $a0, $ra, 0x60	# filename. If you change this value, move it in the savegame
 li $a1, 1
+jal 0x08A885C8		# sceIoOpen
 li $a2, 31
-jal 0x08A885C8		/* sceIoOpen */
-nop
-
-move $a0, $v0		/* set the return value of the function for arg0 of the next function */
-
-lui $a1, 0x08D2 	/* arg1 is 0x08D20000, load address of the binary file */
-lui $a2, 1		/* arg2, read 0x10000 bytes from the file */
-jal 0x08A88578      /* sceIoRead */
-move $s0, $a0		/* backup $a0 value */
-
-jal 0x08A88590      /* sceIoClose */
-move $a0, $s0		/* restore $a0 value */
-
-lui $a0, 0x08D2
-lui $a1, 0x1
-jal 0x08A887C0		/* sceKernelDcacheWritebackInvalidateAll */
-nop
-
-nop
-nop
-li $a0, 0x08D20000
-jr $a0
-nop
-
+ 
+move $s0, $v0		# backup $v0 value
+move $a0, $v0		# set the return value of the function for arg0 of the next function
+lui $a1, 0x08D2 	# arg1 is 0x08D20000, load address of the binary file
+jal 0x08A88578		# sceIoRead
+lui $a2, 1		# arg2, read 0x10000 bytes from the file
+ 
+jal 0x08A88590		# sceIoClose
+move $a0, $s0		# restore $v0 value
+ 
+j 0x08A887C0		# sceKernelDcacheWritebackInvalidateAll
+lui $ra, 0x08D2 	# return address is 0x08D20000, load address of the binary file
