2d41d30abbe96c694ea84cf038515b043d17c80f
root.3.173210@gmail.com <root.3.173210@gmail.com@ae707b24-f6f8-a420-3b91-9542b9ad9fd8>
2014-09-13 13:50:24 +0000

    Update loader/bruteforce.s
    git-svn-id: https://valentine-hbl.googlecode.com/svn/trunk@304 ae707b24-f6f8-a420-3b91-9542b9ad9fd8

--- loader/bruteforce.s
+++ loader/bruteforce.s
@@ -11,8 +11,11 @@ freemem_bruteforce:
 	lui	$a3, %hi(sceKernelGetBlockHeadAddr)
 	lw	$a2, %lo(sceKernelGetBlockHeadAddr) + 4($a3)
 
-	lui	$a3, %hi(loop) + 0x40000000
-	sw	$a2, %lo(loop)($a3)
+	lui	$a3, %hi($loop) + 0x40000000
+	sw	$a2, %lo($loop)($a3)
+
+	lui	$a3, %hi($loop)
+	cache	0x14, %lo($loop)($a3)
 
 	addiu	$sp, $sp, -12
 	sw	$s0, 0($sp)
@@ -22,10 +25,10 @@ freemem_bruteforce:
 	move	$s0, $a0
 	move	$s1, $a1
 
-loop:
+$loop:
 	nop
 	addiu	$s0, $s0, 1
-	bnel	$v0, $s1, loop
+	bnel	$v0, $s1, $loop
 	move	$a0, $s0
 
 	addiu	$a0, $s0, -1
